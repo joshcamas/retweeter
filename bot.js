fae6c2c445fbc697be302a831aaa97c816fcfddc
Josh Steinhauer <joshcamas@gmail.com>
2019-06-16 12:35:53 -0700

    Fixed issue with exports

--- bot.js
+++ bot.js
@@ -1,103 +1,108 @@
-
-var Twit = require("twit");
-var TwitterBot = require("node-twitterbot").TwitterBot;
-
-var lastscan = -1;
-
-var BOT_SEARCH_PHRASE = process.env.BOT_SEARCH_PHRASE;
-var BOT_CONSUMER_KEY = process.env.BOT_CONSUMER_KEY;
-var BOT_CONSUMER_SECRET = process.env.BOT_CONSUMER_SECRET;
-var BOT_ACCESS_TOKEN = process.env.BOT_ACCESS_TOKEN;
-var BOT_ACCESS_TOKEN_SECRET = process.env.BOT_ACCESS_TOKEN_SECRET;
-
-var Bot = new Twit({
- consumer_key: BOT_CONSUMER_KEY,
- consumer_secret: BOT_CONSUMER_SECRET,
- access_token: BOT_ACCESS_TOKEN,
- access_token_secret: BOT_ACCESS_TOKEN_SECRET
-});
-
-console.log('Setting up Bot');
-
-function RetweetStatus(status,onComplete) 
+exports.Bot = function() 
 {
-	var id = {
-		id : status.id_str
-	}
-	
-	//Check if status has already been favorited or retweeted
-	if(status.favorited || status.retweeted) {
-		onComplete(false);
-		return;
-	}
-		
-	console.log(status.text)
-	
-	Bot.post('favorites/create', id, BotFavorited);
-	
-	function BotFavorited(error,response) 
+	this.BOT_SEARCH_PHRASE = process.env.BOT_SEARCH_PHRASE;
+	this.BOT_CONSUMER_KEY = process.env.BOT_CONSUMER_KEY;
+	this.BOT_CONSUMER_SECRET = process.env.BOT_CONSUMER_SECRET;
+	this.BOT_ACCESS_TOKEN = process.env.BOT_ACCESS_TOKEN;
+	this.BOT_ACCESS_TOKEN_SECRET = process.env.BOT_ACCESS_TOKEN_SECRET;
+
+
+	this.Twit = require("twit");
+	this.TwitterBot = require("node-twitterbot").TwitterBot;
+
+	this.lastscan = -1;
+
+	this.Bot = new this.Twit({
+	 consumer_key: this.BOT_CONSUMER_KEY,
+	 consumer_secret: this.BOT_CONSUMER_SECRET,
+	 access_token: this.BOT_ACCESS_TOKEN,
+	 access_token_secret: this.BOT_ACCESS_TOKEN_SECRET
+	});
+
+	console.log('Setting up Bot');
+
+	this.RetweetStatus = function(status,onComplete) 
 	{
-		if (error) {
-			console.log('Bot could not favorite, : ' + error);
-			onComplete(false);
-			return
+		var id = {
+			id : status.id_str
 		}
-		else {
-			console.log('Bot favorited : ' + id.id);
+		
+		//Check if status has already been favorited or retweeted
+		if(status.favorited || status.retweeted) {
+			onComplete(false);
+			return;
 		}
 			
-		Bot.post('statuses/retweet/:id', id, BotRetweeted);
+		console.log(status.text)
 		
-		function BotRetweeted(error, response) {
+		this.Bot.post('favorites/create', id, BotFavorited);
+		
+		function BotFavorited(error,response) 
+		{
 			if (error) {
-				console.log('Bot could not retweet, : ' + error);
+				console.log('Bot could not favorite, : ' + error);
 				onComplete(false);
 				return
 			}
 			else {
-				console.log('Bot retweeted : ' + id.id);
-				onComplete(true);
-				return	
+				console.log('Bot favorited : ' + id.id);
 			}
-		}	
-	}
-}
-
-function InitiateRetweet() {
-
-	console.log('Starting Retweet')
-
-	var query = {
-		q: BOT_SEARCH_PHRASE,
-		result_type: "recent"
-	}
-
-	Bot.get('search/tweets', query, BotGotLatestTweet);
-
-	function BotGotLatestTweet (error, data, response) {
-		if (error) {
-			console.log('Bot could not find latest tweet, : ' + error);
+				
+			Bot.post('statuses/retweet/:id', id, BotRetweeted);
+			
+			function BotRetweeted(error, response) {
+				if (error) {
+					console.log('Bot could not retweet, : ' + error);
+					onComplete(false);
+					return
+				}
+				else {
+					console.log('Bot retweeted : ' + id.id);
+					onComplete(true);
+					return	
+				}
+			}	
 		}
-		else {
+	}
 
-			function ScanTweet(index) {
-				
-				if(index >= data.statuses.length)
-					return;
+	this.InitiateRetweet = Function() {
+
+		console.log('Starting Retweet')
+
+		var query = {
+			q: this.BOT_SEARCH_PHRASE,
+			result_type: "recent"
+		}
+
+		this.Bot.get('search/tweets', query, BotGotLatestTweet);
+
+		function BotGotLatestTweet (error, data, response) {
+			if (error) {
+				console.log('Bot could not find latest tweet, : ' + error);
+			}
+			else {
+
+				function ScanTweet(index) {
 					
-				var status = data.statuses[index];
-				
-				function OnTweetComplete(success) {
-					if(success) 
-						ScanTweet(index+1);
-					else
+					if(index >= data.statuses.length)
 						return;
+						
+					var status = data.statuses[index];
+					
+					function OnTweetComplete(success) {
+						if(success) 
+							ScanTweet(index+1);
+						else
+							return;
+					}
+					
+					this.RetweetStatus(status,OnTweetComplete);
 				}
 				
-				RetweetStatus(status,OnTweetComplete);
+				ScanTweet(0);
 			}
-			
-			ScanTweet(0);
 		}
 	}
+		
+	
 }
--- clock.js
+++ clock.js
@@ -1,18 +1,20 @@
-var bot = require('./bot.js');
-
+var bot = require('bot.js').Bot;
 var CronJob = require('cron').CronJob;
+
+var botInstance = new bot();
 
 console.log('Setting up Clock');
 
 function onTick() {
 	console.log('Tick');
-	bot.InitiateRetweet()
+	botInstance.InitiateRetweet()
 }
 
 var job = new CronJob({
   cronTime: "* * * * *", // Every minute
   onTick: onTick,
-  timeZone: "America/Los_Angeles"
+  timeZone: "America/Los_Angeles",
+  runOnInit: true
 });
 
 console.log('Starting Clock');
