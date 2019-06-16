5f800065f91e1b2e4a53dfb3e1e58837be9f4bb0
Josh Steinhauer <joshcamas@gmail.com>
2019-06-16 12:41:15 -0700

    replaced this with self

--- bot.js
+++ bot.js
@@ -1,27 +1,29 @@
 exports.Bot = function() 
 {
-	this.BOT_SEARCH_PHRASE = process.env.BOT_SEARCH_PHRASE;
-	this.BOT_CONSUMER_KEY = process.env.BOT_CONSUMER_KEY;
-	this.BOT_CONSUMER_SECRET = process.env.BOT_CONSUMER_SECRET;
-	this.BOT_ACCESS_TOKEN = process.env.BOT_ACCESS_TOKEN;
-	this.BOT_ACCESS_TOKEN_SECRET = process.env.BOT_ACCESS_TOKEN_SECRET;
+	var self = this;
+	
+	self.BOT_SEARCH_PHRASE = process.env.BOT_SEARCH_PHRASE;
+	self.BOT_CONSUMER_KEY = process.env.BOT_CONSUMER_KEY;
+	self.BOT_CONSUMER_SECRET = process.env.BOT_CONSUMER_SECRET;
+	self.BOT_ACCESS_TOKEN = process.env.BOT_ACCESS_TOKEN;
+	self.BOT_ACCESS_TOKEN_SECRET = process.env.BOT_ACCESS_TOKEN_SECRET;
 
 
-	this.Twit = require("twit");
-	this.TwitterBot = require("node-twitterbot").TwitterBot;
+	self.Twit = require("twit");
+	self.TwitterBot = require("node-twitterbot").TwitterBot;
 
-	this.lastscan = -1;
+	self.lastscan = -1;
 
-	this.Bot = new this.Twit({
-	 consumer_key: this.BOT_CONSUMER_KEY,
-	 consumer_secret: this.BOT_CONSUMER_SECRET,
-	 access_token: this.BOT_ACCESS_TOKEN,
-	 access_token_secret: this.BOT_ACCESS_TOKEN_SECRET
+	self.Bot = new self.Twit({
+	 consumer_key: self.BOT_CONSUMER_KEY,
+	 consumer_secret: self.BOT_CONSUMER_SECRET,
+	 access_token: self.BOT_ACCESS_TOKEN,
+	 access_token_secret: self.BOT_ACCESS_TOKEN_SECRET
 	});
 
 	console.log('Setting up Bot');
 
-	this.RetweetStatus = function(status,onComplete) 
+	self.RetweetStatus = function(status,onComplete) 
 	{
 		var id = {
 			id : status.id_str
@@ -35,7 +37,7 @@ exports.Bot = function()
 			
 		console.log(status.text)
 		
-		this.Bot.post('favorites/create', id, BotFavorited);
+		self.Bot.post('favorites/create', id, BotFavorited);
 		
 		function BotFavorited(error,response) 
 		{
@@ -65,16 +67,16 @@ exports.Bot = function()
 		}
 	}
 
-	this.InitiateRetweet = function() {
+	self.InitiateRetweet = function() {
 
 		console.log('Starting Retweet')
 
 		var query = {
-			q: this.BOT_SEARCH_PHRASE,
+			q: self.BOT_SEARCH_PHRASE,
 			result_type: "recent"
 		}
 
-		this.Bot.get('search/tweets', query, BotGotLatestTweet);
+		self.Bot.get('search/tweets', query, BotGotLatestTweet);
 
 		function BotGotLatestTweet (error, data, response) {
 			if (error) {
@@ -96,7 +98,7 @@ exports.Bot = function()
 							return;
 					}
 					
-					this.RetweetStatus(status,OnTweetComplete);
+					self.RetweetStatus(status,OnTweetComplete);
 				}
 				
 				ScanTweet(0);
